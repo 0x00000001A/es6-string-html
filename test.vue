aa8bbcafc9cd6734fad933daf10ce826d4e69681
Viktor Novikov <viktor.novikov@brandmaker.com>
2020-10-06 15:02:55 +0700

    Rollback changes about highlighting w/o comment. Bump version

--- test.vue
+++ test.vue
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <script>
-        let variable = /*html*/ `
-            <div>Test</div>${Window}
-        `
+<template>
+  
+</template>
+
+<script>
+export default {
+    test: /*html*/`<div>OK</div>`,
+    test2() {
+        let variable = /*html*/ `<div>Test</div>${Window}`
 
         variable = /*html*/`<div>Test</div>${window}`
 
@@ -30,6 +26,11 @@
         `<div>Something</div>${window}`
         )
         fn(html`<div>OK</div>`)
-    </script>
-</body>
-</html>
+    },
+    test: 'asdas'
+}
+</script>
+
+<style>
+
+</style>
